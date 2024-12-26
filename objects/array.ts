b2c09ba6531d43f50300499b81394b85d714b039
Natã Tidioli Girotto <natatidioligirotto@gmail.com>
2024-12-26 17:11:54 -0300

    first commit

---
+++ objects/array.ts
@@ -0,0 +1,6 @@
+const listaDeCompras: string[] = ["laranja", "pera", "maça", "abacate", "macarrao"];
+
+listaDeCompras.push("Salsichaaa");
+
+console.log(listaDeCompras.length);
+console.log(listaDeCompras[4]);
---
+++ tipos-primitivos/main.ts
@@ -0,0 +1,17 @@
+
+const a :number = 2;
+const b :number = 8;
+const d : boolean = true;
+const c : string = "Santos";
+const e : string = "Marquinhos jotaquest";
+
+
+function soma(a: number, b: number): number {
+    return a + b;    
+};
+function sumName(e: string, c: string): string{
+    return e + c;
+}
+
+console.log(soma(a, b), sumName(e, c), e.toLowerCase() );
+
